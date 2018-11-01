8e0b7ceb96bd0f806a1016213e6218d4b862175c
Michael Perrotte <michael.perrotte@resolver.com>
2018-10-31 21:52:42 -0400

    added symlinking for vscode config files

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -8,6 +8,14 @@ ln -sfv "$DOTFILES_DIR/dots/.bashrc" ~
 # ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
 # ln -sfv "$DOTFILES_DIR/dots/.inputrc" ~
 
+# VSCODE Snippet mapping
+vscode_snippets=$(ls ${DOTFILES_DIR}/vscode/snippets)
+for snippet in ${vscode_snippets}; do
+  ln -sfv "${DOTFILES_DIR}/vscode/snippets/${snippet}" "${HOME}/Library/Application Support/Code/User/snippets"
+done
+ln -sfv "${DOTFILES_DIR}/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User"
+ln -sfv "${DOTFILES_DIR}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User"
+
 # Exmaples for mapping
 # ln -sfv "$DOTFILES_DIR/editors/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
 # ln -sfv "$DOTFILES_DIR/editors/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
