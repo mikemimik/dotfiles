42b004be77e41dfdea28babd854b691a188a29bc
Michael Perrotte <michael.perrotte@resolver.com>
2019-01-03 14:19:03 -0500

    uncommented code that should actually be running...

--- install/install.sh
+++ install/install.sh
@@ -12,29 +12,29 @@ echo ""
 . "$DOTFILES_DIR/install/brew-cask.sh"
 
 # Bunch of symlinks
-# echo ">>> Symlinking files..."
-# echo ""
-# . "$DOTFILES_DIR/install/symlinks.sh"
+echo ">>> Symlinking files..."
+echo ""
+. "$DOTFILES_DIR/install/symlinks.sh"
 
 # Add keys from keychain to ssh agent
-# ssh-add -A 2>/dev/null;
+ssh-add -A 2>/dev/null;
 
 # Clear cache
-# echo ">>> Cleaning up..."
-# echo ""
-# . "$DOTFILES_DIR/bin/dot-cli" clean
+echo ">>> Cleaning up..."
+echo ""
+. "$DOTFILES_DIR/bin/dot-cli" clean
 
 # Setup dock icons
-# echo ">>> Executing macOS dock script..."
-# echo ""
-# . "$DOTFILES_DIR/macos/dock.sh"
+echo ">>> Executing macOS dock script..."
+echo ""
+. "$DOTFILES_DIR/macos/dock.sh"
 
 # Setup apps
-# echo ">>> Executing app specific scripts..."
-# echo ""
-# . "$DOTFILES_DIR/install/apps.sh"
+echo ">>> Executing app specific scripts..."
+echo ""
+. "$DOTFILES_DIR/install/apps.sh"
 
 # NOTE(mperrotte): this must execute last, because it prompts for a restart
 # Setup macos defaults (causes restart, must be last action)
-# echo "Executing macOS defaults script..."
-# . "$DOTFILES_DIR/macos/defaults.sh"
+echo "Executing macOS defaults script..."
+. "$DOTFILES_DIR/macos/defaults.sh"
