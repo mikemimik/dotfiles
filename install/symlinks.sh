8ab8cae9da083bd7d6957e68dc7fdd2ae5b67c4d
Michael Perrotte <michael.perrotte@resolver.com>
2019-02-25 11:59:10 -0500

    added vimrc config file and symlink

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -5,6 +5,7 @@ ln -sfv "$DOTFILES_DIR/dots/.bash_functions" ~
 ln -sfv "$DOTFILES_DIR/dots/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/dots/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/dots/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/dots/.vimrc" ~
 
 # ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
 # ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
