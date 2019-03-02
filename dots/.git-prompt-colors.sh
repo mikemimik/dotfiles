df0d286e1ff720ab765ff942a6b81081483315d4
Michael Perrotte <michael.perrotte@resolver.com>
2019-03-02 18:46:59 -0500

    feat(dots): updated git prompt style; using new line

--- dots/.git-prompt-colors.sh
+++ dots/.git-prompt-colors.sh
@@ -2,7 +2,7 @@
 # it uses the default openSUSE bash prompt style with exit status
 
 override_git_prompt_colors() {
-  GIT_PROMPT_THEME_NAME="Single_line_nono031"
+  GIT_PROMPT_THEME_NAME="Custom"
   GIT_PROMPT_BRANCH="${Cyan}"
   GIT_PROMPT_MASTER_BRANCH="${GIT_PROMPT_BRANCH}"
   GIT_PROMPT_UNTRACKED=" ${Cyan}…${ResetColor}"
@@ -12,8 +12,8 @@ override_git_prompt_colors() {
   GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${BoldGreen}\h:${BoldBlue}\W${ResetColor}"
   GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_ ${BoldRed}\h:${BoldBlue}\W${ResetColor}"
 
-  GIT_PROMPT_END_USER="${ResetColor}> "
+  GIT_PROMPT_END_USER="${ResetColor}\n> "
   GIT_PROMPT_END_ROOT=" # ${ResetColor}"
 }
 
-reload_git_prompt_colors "Single_line_nono031"
+reload_git_prompt_colors "Custom"
