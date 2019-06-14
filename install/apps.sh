ae41942fcec849f626c7be2f5703ebb57565362a
afrolionx <mike@mikecorp.ca>
2019-06-14 17:04:45 -0500

    (install): updated apps script to iterate over app folders and execute install script

--- install/apps.sh
+++ install/apps.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-# VSCODE
-. ${DOTFILES_DIR}/apps/vscode/extensions.sh
+#### README ###################################################################
+#
+# This file will iterate over all the folders in the `apps/` directory. It will
+# execute the `install.sh` file in that folder (if it exists).
+#
+###############################################################################
+
+apps=$(ls ${DOTFILES_DIR}/apps)
+
+for app in ${apps}; do
+  install_file=${DOTFILES_DIR}/apps/${app}/install.sh
+  [ -f ${install_file} ] && . ${install_file}
+done
