d8a6ef78671f45099670256455aca9c97a0b75c3
Michael Perrotte <mike@npmjs.com>
2019-11-20 13:58:09 -0500

    feat(dots,install): add configuration and script files; updated install script

--- install/dots.sh
+++ install/dots.sh
@@ -12,8 +12,9 @@ ln -sfv "$DOTFILES_DIR/dots/.bash_functions" ~
 ln -sfv "$DOTFILES_DIR/dots/.bash_aliases" ~
 ln -sfv "$DOTFILES_DIR/dots/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/dots/.bashrc" ~
-ln -sfv "$DOTFILES_DIR/dots/.vimrc" ~
 
-# ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/dots/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/dots/.iterm2_shell_integration.sh" ~
 # ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
 # ln -sfv "$DOTFILES_DIR/dots/.inputrc" ~
