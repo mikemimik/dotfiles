0f709190e03c2eea9f8a43bed1ef6eae777b6c2d
Michael Perrotte <michael.perrotte@okta.com>
2023-05-09 00:39:50 -0400

    fix(macos): comment out power config

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -200,30 +200,30 @@ defaults write com.apple.dock autohide -bool true
 # Power                                                                       #
 ###############################################################################
 
-# Set standby delay to 24 hours (default is 1 hour)
-sudo pmset -a standbydelay 86400
+# # Set standby delay to 24 hours (default is 1 hour)
+# sudo pmset -a standbydelay 86400
 
-# Computer sleep: 60 min
-sudo pmset -b sleep 60 # Battery
-sudo pmset -c sleep 30 # Power Adapter
+# # Computer sleep: 60 min
+# sudo pmset -b sleep 60 # Battery
+# sudo pmset -c sleep 30 # Power Adapter
 
-# Display sleep: 60 min
-sudo pmset -b displaysleep 60 # Battery
-sudo pmset -c displaysleep 60 # Power Adapter
+# # Display sleep: 60 min
+# sudo pmset -b displaysleep 60 # Battery
+# sudo pmset -c displaysleep 60 # Power Adapter
 
-# Put the hard disk(s) to sleep when possible: 60 min
-sudo pmset -b disksleep 60 # Battery
-sudo pmset -c disksleep 60 # Power Adapter
+# # Put the hard disk(s) to sleep when possible: 60 min
+# sudo pmset -b disksleep 60 # Battery
+# sudo pmset -c disksleep 60 # Power Adapter
 
-# Slightly dim the display when using this power source
-sudo pmset -b lessbright 1
+# # Slightly dim the display when using this power source
+# sudo pmset -b lessbright 1
 
-# Automatically reduce brightness before display goes to sleep
-sudo pmset -b halfdim 1 # Battery
-sudo pmset -c halfdim 1 # Power Adapter
+# # Automatically reduce brightness before display goes to sleep
+# sudo pmset -b halfdim 1 # Battery
+# sudo pmset -c halfdim 1 # Power Adapter
 
-# Wake for network access
-sudo pmset -c womp 0
+# # Wake for network access
+# sudo pmset -c womp 0
 
 ###############################################################################
 # Do some clean up work.                                                      #
