691b6b9fcd14f02378e29325f138386e00520dc0
Michael Perrotte <mike@mikecorp.ca>
2023-10-06 00:29:22 -0400

    feat(apps,nvim): update init config

--- apps/nvim/lua/mikemimik/init.lua
+++ apps/nvim/lua/mikemimik/init.lua
@@ -13,11 +13,11 @@ vim.opt.mouse = "a"
 vim.opt.backspace = { "indent", "eol", "start" }
 
 -- Indentation
-vim.opt.tabstop = 2 -- <Tab> appears as 2 spaces
-vim.opt.shiftwidth = 2 -- >>, << shift line by 2 spaces
-vim.opt.softtabstop = 2 -- <Tab> behaves like 2 spaces when editing
+vim.opt.tabstop = 2       -- <Tab> appears as 2 spaces
+vim.opt.shiftwidth = 2    -- >>, << shift line by 2 spaces
+vim.opt.softtabstop = 2   -- <Tab> behaves like 2 spaces when editing
 vim.opt.autoindent = true -- continue indentation to new line
-vim.opt.expandtab = true -- <Tab> inserts spaces
+vim.opt.expandtab = true  -- <Tab> inserts spaces
 
 -- Look & Feel
 vim.opt.number = true
@@ -37,7 +37,7 @@ vim.opt.listchars = {
   eol = "$",
   space = ".",
 }
-vim.opt.scrolloff = 10 -- padding between cursor and top/bottom of window
+vim.opt.scrolloff = 10    -- padding between cursor and top/bottom of window
 vim.opt.splitright = true -- prefer vsplitting to the right
 vim.opt.splitbelow = true -- prefer splitting below
 vim.opt.textwidth = 80
@@ -52,16 +52,18 @@ vim.opt.hlsearch = true
 
 -- AUTOCMDs
 local augroup = vim.api.nvim_create_augroup
-baseGroup = augroup("base", {})
+local baseGroup = augroup("base", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 
+-- NOTE: trims whitespace from buffer
 autocmd({ "BufWritePre" }, {
   group = baseGroup,
   pattern = "*",
   command = "%s/\\s\\+$//e",
 })
 
+-- NOTE: format "env" files as `sh` syntax
 autocmd({ "BufNewFile", "BufRead" }, {
   group = baseGroup,
   pattern = "*.env.*",
@@ -70,9 +72,26 @@ autocmd({ "BufNewFile", "BufRead" }, {
   end,
 })
 
+-- NOTE: auto equalise panes when vim is resized
 autocmd({ "VimResized" }, {
   group = baseGroup,
   pattern = "*",
   command = "wincmd =",
+})
+
+-- NOTE: configure TreesitterContextBottom
+autocmd({ "VimEnter" }, {
+  group = baseGroup,
+  pattern = "*",
+  command = "highlight TreesitterContextBottom gui=underline guisp=Grey",
+})
+
+-- NOTE: fix for insert bug
+autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
 })
 -- print("mikemimik.init -- loaded")
