768a427a65e90d9daaeb678302d17edf5be5886d
Michael Perrotte <mike@mikecorp.ca>
2023-10-06 00:30:03 -0400

    feat(apps,nvim): update plugins config

--- apps/nvim/lua/mikemimik/plugins.lua
+++ apps/nvim/lua/mikemimik/plugins.lua
@@ -17,6 +17,7 @@ return packer.startup(function(use)
   -- Default
   use("wbthomason/packer.nvim")
   use("tjdevries/vlog.nvim")
+  use("nvim-lua/plenary.nvim")
 
   -- Visualizations
   use("shaunsingh/seoul256.nvim")
@@ -24,48 +25,106 @@ return packer.startup(function(use)
   -- requires patched font with glyps
   use({
     "akinsho/bufferline.nvim",
-    tag = "*",
-    requires = 'nvim-tree/nvim-web-devicons'
+    tag = "v4.*",
+    requires = "nvim-tree/nvim-web-devicons"
   })
   use("Yggdroot/indentLine")
   use("nvim-lualine/lualine.nvim")
   use({
     "ellisonleao/glow.nvim"
-    -- requires 'glow' (brew install)
+    -- requires "glow" (brew install)
   })
   use({ "xiyaowong/transparent.nvim" })
+  use({
+    'samueljoli/hurl.nvim',
+    config = function()
+      require('hurl').setup({
+        comment          = "#ebc021", -- default => Comment
+        method           = "#fffc58", -- default => Statement
+        url              = "#fffc58", -- default => Underlined
+        version          = "#032ea7", -- default => Statement
+        status           = "#032ea7", -- default => Number
+        section          = "#032ea7", -- default => Statement
+        operators        = "#c592ff", -- default => Operator
+        string           = "#032ea7", -- default => String
+        query            = "#d57bff", -- default => Identifier
+        filter           = "#032ea7", -- default => Operator
+        predicate        = "#032ea7", -- default => Operator
+        template         = "#032ea7", -- default => Identifier
+        escapeQuote      = "#032ea7", -- default => SpecialChar
+        escapeNumberSign = "#032ea7", -- default => SpecialChar
+      })
+    end
+  })
 
   -- Functionality
   use({
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate"
-    -- requires 'tree-sitter' (brew install)
+    -- requires "tree-sitter" (brew install)
   })
-  use({ "nvim-telescope/telescope.nvim",
+  use({
+    "nvim-telescope/telescope.nvim",
     requires = { { "nvim-lua/plenary.nvim" } }
-    -- requires 'ripgrep' (brew install)
+    -- requires "ripgrep" (brew install)
   })
   use("nvim-telescope/telescope-file-browser.nvim")
   use("tpope/vim-commentary")
   use("tpope/vim-fugitive")
   use("kazhala/close-buffers.nvim")
+  use({
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup({
+        enable = true,
+        max_lines = 0,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = "outer",
+        mode = "cursor",
+        -- separator = "-",
+        zindex = 20,
+      })
+    end,
+  })
 
   -- LSP Configuration
   use({
-    "neovim/nvim-lspconfig"
-    -- requires 'lua-language-server' (brew install)
-    -- requires 'typescript-language-server' (npm i -g)
-  }) -- LSP
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lua")
-  use("L3MON4D3/LuaSnip")
-  use({
-    -- TODO: figure out how to utilise correctly
-    "glepnir/lspsaga.nvim",
-    branch = "main",
+    "VonHeikemen/lsp-zero.nvim",
+    -- TODO: September 20, 2023 this will be set
+    -- branch = "v3.x",
+    branch = "dev-v3",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" }, -- Required
+      -- { "williamboman/mason.nvim" },           -- Optional
+      -- { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },     -- Required
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-nvim-lsp" }, -- Required
+      { "hrsh7th/cmp-nvim-lua" },
+
+      -- Snippets
+      { "L3MON4D3/LuaSnip" }, -- Required
+    }
   })
+  -- use({
+  --   "mrded/nvim-lsp-notify",
+  --   config = function()
+  --     require("lsp-notify").setup({})
+  --   end
+  -- })
+  -- use("hrsh7th/cmp-nvim-lua")
+  -- use("L3MON4D3/LuaSnip") -- to be deleted
+  -- use({
+  --   -- TODO: figure out how to utilise correctly
+  --   "nvimdev/lspsaga.nvim",
+  --   after = "nvim-lspconfig",
+  --   branch = "main",
+  -- })
   use("jose-elias-alvarez/null-ls.nvim")
   use("onsails/lspkind-nvim")
 
