8c009fea2c7da8642e05cd40742b8f586fbeb3dc
Michael Perrotte <mike@mikecorp.ca>
2023-10-06 00:26:31 -0400

    fix(apps,nvim): update colors

--- apps/nvim/after/plugin/colors.rc.lua
+++ apps/nvim/after/plugin/colors.rc.lua
@@ -1,9 +1,10 @@
 -- print("after/plugin.color.rc -- loading")
+
 vim.g.seoul256_light_background = 253
 vim.g.seoul256_background = 234
 
 vim.g.colors_name = "seoul256"
-
+vim.g.seoul256_italic_comments = true
 vim.g.seoul256_borders = true
 vim.g.seoul256_disable_background = true
 vim.g.seoul256_hl_current_line = true
