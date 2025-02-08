5777d9525bd5548303dbdaca45ad58e34c7a23aa
Michael Perrotte <mike@mikecorp.ca>
2025-02-08 17:51:37 -0500

    feat: update setup script

--- setup.sh
+++ setup.sh
@@ -2,24 +2,40 @@
 
 echo "Initial setup of dotfiles..."
 
-if [[ -z ${STOW_FOLDERS} ]]
-then
-  STOW_FOLDERS="ag alacritty bash git hammerspoon karabiner nvm tmux"
+if [[ -z ${DOTFILES} ]]; then
+  DOTFILES="${HOME}/dotfiles"
 fi
 
-if [[ -z ${DOTFILES} ]]
-then
-  DOTFILES="${HOME}/dotfiles"
+# INFO: stow configuration files
+if [[ -z ${STOW_FOLDERS} ]]; then
+  STOW_FOLDERS="ag alacritty bash git hammerspoon karabiner nvm nvim tmux"
 fi
 
 cd "${DOTFILES}" || return
 
-for folder in ${STOW_FOLDERS}
-do
+for folder in ${STOW_FOLDERS}; do
   echo "stow ${folder}"
   stow -D "${folder}"
   stow "${folder}"
 done
 
+# INFO: Setup system fonts
+
+cp -R ${DOTFILES}/fonts/ ~/Library/Fonts/
+
+# INFO: Update dotfiles repo from 'https' to 'ssh'
+cd "${DOTFILES}" || return
+
+IFS='/' read -ra PARTS <<<"$(git remote -vv | awk '{ print $2}')"
+DOMAIN="${PARTS[2]}"
+USERNAME="${PARTS[3]}"
+REPOSITORY="${PARTS[4]}"
+
+git remote set-url origin "git@${DOMAIN}:${USERNAME}/${REPOSITORY}"
+
+# INFO: Setup submodules
+cd "${DOTFILES}" || return
+
+git submodule update --init --recursive
+
 echo "Initial setup of dotfiles complete."
-# echo "Usage: dot-cli --help"
